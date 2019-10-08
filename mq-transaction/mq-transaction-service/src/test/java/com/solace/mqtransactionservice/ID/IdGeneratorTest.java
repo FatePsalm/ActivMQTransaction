de913d2973b46cbcf0518d804adb3e2227b7f051
Solace <545058656@qq.com>
2019-10-08 13:45:11 +0800

    分布式ID

---
+++ mq-transaction/mq-transaction-service/src/test/java/com/solace/mqtransactionservice/ID/IdGeneratorTest.java
@@ -0,0 +1,60 @@
+package com.solace.mqtransactionservice.ID;
+
+import ch.qos.logback.classic.Logger;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class IdGeneratorTest {
+    private final static Logger log = (Logger) LoggerFactory.getLogger(IdGenerator.class);
+
+
+    //@Autowired
+    private IdGenerator idGenerator = new IdGenerator();
+
+    @Test
+    public void testBatchId() {
+        for (int i = 0; i < 100; i++) {
+            String batchId = idGenerator.batchId(1001, 100);
+            log.info("批次号: {}", batchId);
+        }
+    }
+
+    @Test
+    public void testSimpleUUID() {
+        for (int i = 0; i < 100; i++) {
+            String simpleUUID = idGenerator.simpleUUID();
+            log.info("simpleUUID: {}", simpleUUID);
+        }
+    }
+
+    @Test
+    public void testRandomUUID() {
+        for (int i = 0; i < 100; i++) {
+            String randomUUID = idGenerator.randomUUID();
+            log.info("randomUUID: {}", randomUUID);
+        }
+    }
+
+    @Test
+    public void testObjectID() {
+        for (int i = 0; i < 100; i++) {
+            String objectId = idGenerator.objectId();
+            log.info("objectId: {}", objectId);
+        }
+    }
+
+    @Test
+    public void testSnowflakeId() {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(() -> {
+                log.info("分布式 ID: {}", idGenerator.snowflakeId());
+            });
+        }
+        executorService.shutdown();
+    }
+
+}
